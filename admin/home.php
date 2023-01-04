09f60f38f7e867fbf48b2620e0d6169b250c57be
soraa14 <azizadhiaksa@gmail.com>
2023-01-04 14:27:16 +0700

    hapus file ajis / rename home-wip.php

--- admin/home.php
+++ admin/home.php
@@ -1,170 +1,162 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
   <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
-    <meta name="generator" content="Hugo 0.84.0">
-    <title>Admin Panel</title>
-
-    <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/dashboard/">
-
-    
-
-    <!-- Bootstrap core CSS -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
-
-    <style>
-      .bd-placeholder-img {
-        font-size: 1.125rem;
-        text-anchor: middle;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        user-select: none;
-      }
-
-      @media (min-width: 768px) {
-        .bd-placeholder-img-lg {
-          font-size: 3.5rem;
-        }
-      }
-    </style>
-
-    
-    <!-- Custom styles for this template -->
-    <link href="../public/css/dashboard.css" rel="stylesheet">
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/style/style.css" />
+    <link rel="stylesheet" href="../public/css/output.css" />
   </head>
-  <body>
-    
-<header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-  <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">Admin Panel</a>
-  <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="navbar-nav">
-    <div class="nav-item text-nowrap">
-      <a class="nav-link px-3" href="#">Sign out</a>
-    </div>
-  </div>
-</header>
 
-<div class="container-fluid">
-  <div class="row">
-    <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
-      <div class="position-sticky pt-3">
-        <ul class="nav flex-column">
-          <li class="nav-item">
-            <a class="nav-link" aria-current="page" href="home.php">
-              Home
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="profile.php">
-              Profile
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="create-article.php">
-              Create New Article
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="my-article.php">
-            My Articles
-            </a>
-          </li>
+  <body class="bg-primary">
+    <!-- min-[1700px]:grid-cols-3 -->
+    <div class="container-all w-full sm:p-0 sm:grid sm:grid-cols-sidebar min-[1136px]:overflow-hidden">
+      <div class="top-bar max-w-full bg-black fixed top-0 left-0 right-0 py-[2rem] p-standart">
+        <nav class="container w-[90rem] self-start text-left text-white text-xl">Panel Admin Ikuzo Temera</nav>
+      </div>
+
+      <!-- min-[1700px]:col-start-1 -->
+      <header class="col-start-1 w-[15rem] min-h-[100vh] shadow-xl mt-12 hidden sm:block fixed top-0 left-0 bottom-0">
+        <ul class="side-bar tabs my-6 px-8 flex flex-col justify-evenly items-center h-[33rem]">
+          <li class="tab"><a href="#">Home</a></li>
+          <li class="tab"><a href="#">Profile</a></li>
+          <li class="tab"><a href="#">Buat Artikel</a></li>
+          <li class="tab"><a href="#">Artikel Saya</a></li>
         </ul>
+      </header>
 
-        <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
-          
-      </div>
-    </nav>
+      <main class="w-full col-start-2 col-end-5 grid-rows-1 mt-[8rem]">
+        <section id="Home" class="tab-content w-[100%] sm:w-[80%]">
+          <h2 class="text-2xl">Selamat datang <span class="text-slate-500 font-bold">Aeri Uchinaga</span></h2>
 
-    <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-      <h2>Welcome, Nama User!</h2>
-      <div class="table-responsive">
-        <table class="table table-striped table-sm">
-          <thead>
-            <tr>
-              <th scope="col">ID</th>
-              <th scope="col">Title</th>
-              <th scope="col">Category</th>
-              <th scope="col">Tags</th>
-              <th scope="col">Author</th>
-              <th scope="col">Last Edited</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>1</td>
-              <td>Apa itu Cybersecurity?</td>
-              <td>Cybersecurity</td>
-              <td>
-                <span class="badge text-bg-dark m">cybersecurity</span>
-            </td>
-              <td>Asis</td>
-              <td>02 January 2023</td>
-            </tr>
-            <tr>
-              <td>2</td>
-              <td>React 101</td>
-              <td>Programming</td>
-              <td>
-                <span class="badge text-bg-dark m">frontend</span>
-                <span class="badge text-bg-dark m">react</span>
-                <span class="badge text-bg-dark m">javascript</span>
-              </td>
-              <td>Ikhlas</td>
-              <td>01 January 2023</td>
-            </tr>
-            <tr>
-              <td>3</td>
-              <td>Apa itu API?</td>
-              <td>Programming</td>
-              <td>
-                <span class="badge text-bg-dark m">backend</span>
-                <span class="badge text-bg-dark m">api</span>
-                <span class="badge text-bg-dark m">nodejs</span>
-              </td>
-              <td>Ikhlas</td>
-              <td>12 December 2022</td>
-            </tr>
-            <tr>
-              <td>4</td>
-              <td>Robotics 101</td>
-              <td>IOT</td>
-              <td>
-                <span class="badge text-bg-dark m">robotic</span>
-                <span class="badge text-bg-dark m">arduino</span>
-                <span class="badge text-bg-dark m">programming</span>
-              </td>
-              <td>Roy</td>
-              <td>24 May 2022</td>
-            </tr>
-            <tr>
-              <td>5</td>
-              <td>Apa itu XSS?</td>
-              <td>Cybersecurity</td>
-              <td>
-                <span class="badge text-bg-dark m">pentest</span>
-                <span class="badge text-bg-dark m">offensive</span>
-                <span class="badge text-bg-dark m">bugbounty</span>
-              </td>
-              <td>Asis</td>
-              <td>12 February 2022</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </main>
-  </div>
-</div>
+          <table class="w-full overflow-scroll min-[1136px]:overflow-hidden mt-8 table-auto border-collapse">
+            <!-- Baris tabel -->
+            <thead class="">
+              <td class="px-2 text-center">ID</td>
+              <td class="p-6">Judul</td>
+              <td class="text-center p-3">Kategori</td>
+              <td class="text-center p-3">Tag</td>
+              <td class="text-center p-3">Author</td>
+              <td class="text-center p-3">Dibuat</td>
+            </thead>
 
+            <tbody>
+              <tr>
+                <!-- Kolom isi -->
+                <td class="text-center border-t border-b border-slate-300 text-sm p-3 lg:text-lg">1</td>
+                <td class="border-t border-b border-slate-300 text-sm lg:text-lg">Cara membuat hover di tailwindcss</td>
+                <td class="border-t border-b border-slate-300 text-sm p-3 lg:text-lg">Programming</td>
+                <td class="border-t border-slate-300 text-sm p-3 tags">
+                  <span class="html">Html</span>
+                  <span class="css">CSS</span>
+                </td>
+                <td class="border-t border-b border-slate-300 text-sm p-3 lg:text-lg">siGanteng</td>
+                <td class="border-t border-b border-slate-300 text-sm p-3 lg:text-lg">2 Februari 2023</td>
+              </tr>
+              <tr>
+                <td class="pl-2 text-center border border-slate-300 text-sm p-3 lg:text-lg">2</td>
+                <td class="border-t border-b border-slate-300 text-sm lg:text-lg">Cara membobol bank menggunakan termux</td>
+                <td class="border-t border-b border-slate-300 text-sm lg:text-lg">CyberSecurity</td>
+                <td class="border-t border-b border-slate-300 text-sm p-3 tags">
+                  <span class="html">Html</span>
+                  <span class="css">CSS</span>
+                  <span class="js">JS</span>
+                </td>
+                <td class="border-t border-b border-slate-300 text-sm lg:text-lg">Si ganteng</td>
+                <td class="border-t border-b border-slate-300 text-sm lg:text-lg">2 Februari 2023</td>
+              </tr>
+              <tr>
+                <td class="text-center border-t border-b border-slate-300 text-sm p-3 lg:text-lg">3</td>
+                <td class="border-t border-b border-slate-300 text-sm lg:text-lg">Cara membuat website</td>
+                <td class="p-3 border-t border-b border-slate-300 text-sm lg:text-lg">Programming</td>
+                <td class="p-3 border-t border-slate-300 text-sm tags">
+                  <span class="html">Html</span>
+                  <span class="css">CSS</span>
+                  <span class="js">JS</span>
+                  <span class="php">PHP</span>
+                </td>
+                <td class="border-t border-b border-slate-300 text-sm lg:text-lg">Si ganteng</td>
+                <td class="border-t border-b border-slate-300 text-sm lg:text-lg">10 Februari 2023</td>
+              </tr>
+              <tr>
+                <td class="p-3 text-center border-t border-b border-slate-300 text-sm lg:text-lg">4</td>
+                <td class="border-t border-b border-slate-300 text-sm lg:text-lg">Kenapa kita harus pake Python untuk semua project??</td>
+                <td class="p-3 border-t border-b border-slate-300 text-sm lg:text-lg">Programming</td>
+                <td class="border-t border-slate-300 text-sm p-3 tags">
+                  <span class="php">Django</span>
+                  <span class="python">Python</span>
+                </td>
+                <td class="border-t border-b border-slate-300 text-sm lg:text-lg">Si ganteng</td>
+                <td class="border-t border-slate-300 text-sm lg:text-lg">10 Februari 2023</td>
+              </tr>
+              <tr>
+                <td class="p-3 text-center border-t border-b border-slate-300 text-sm">5</td>
+                <td class="border-t border-b border-slate-300 text-sm lg:text-lg">Robotics 101</td>
+                <td class="p-3 border-t border-b border-slate-300 text-sm lg:text-lg">IoT</td>
+                <td class="border-t border-slate-300 text-sm p-3 tags">
+                  <span class="Robotics">Robotics</span>
+                  <span class="Robotics">Arduino</span>
+                </td>
+                <td class="border-t border-b border-slate-300 text-sm lg:text-lg">Si ganteng</td>
+                <td class="border-t border-b border-slate-300 text-sm lg:text-lg">10 Februari 2023</td>
+              </tr>
+              <tr>
+                <td class="p-3 text-center border-t border-b border-slate-300 text-sm lg:text-lg">6</td>
+                <td class="border-t border-b border-slate-300 text-sm lg:text-lg">Kenapa kita harus pake Python untuk semua project??</td>
+                <td class="p-3 border-t border-b border-slate-300 text-sm lg:text-lg">Programming</td>
+                <td class="border-t border-b border-slate-300 text-sm p-3 tags">
+                  <span class="php">Django</span>
+                  <span class="python">Python</span>
+                </td>
+                <td class="border-t border-b border-slate-300 text-sm lg:text-lg">Si ganteng</td>
+                <td class="border-t border-b border-slate-300 text-sm lg:text-lg">10 Februari 2023</td>
+              </tr>
+            </tbody>
+          </table>
+        </section>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- ========== 
+            *Profile PAGE 
+            =========== -->
 
-      <script src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js" integrity="sha384-uO3SXW5IuS1ZpFPKugNNWqTZRRglnUJK6UAZ/gxOX80nxEkN9NcGZTftn6RzhGWE" crossorigin="anonymous"></script><script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.min.js" integrity="sha384-zNy6FEbO50N+Cg5wap8IKA4M/ZnLJgzc6w2NqACZaK0u0FXfOWRRJOnQtpZun8ha" crossorigin="anonymous"></script>
-      <script src="../src/js/dashboard.js"></script>
+        <section class="tab-content mx-auto profile lg:ml-9 active lg:w-[20%]">
+          <h2 class="text-2xl font-bold mb-10 text-center">Profile Admin</h2>
+
+          <div class="card w-full sm:w-[20rem] py-5 lg:self-start flex flex-col justify-center items-center shadow-xl border-4">
+            <div class="avatar card-img w-[10rem] h-[5rem]" id="avatar">
+              <img src="https://avatars.dicebear.com/api/adventurer/ikhsan.svg" alt="avatar" class="block" id="avatarnya" />
+            </div>
+
+            <div class="card-titles text-center mt-16">
+              <h2 class="font-semibold">Hi, <span>Admin</span></h2>
+              <h2 class="text-slate-500 font-semibold">Edit Profile</h2>
+
+              <form class="form mt-6 flex flex-col gap-y-5">
+                <div class="boxInput border border-slate-900">
+                  <label for="username">
+                    <input type="text" id="username" class="username py-2 px-4" placeholder="Username User" />
+                  </label>
+                </div>
+                <div class="boxInput border border-slate-900">
+                  <label for="password">
+                    <input type="password" id="password" class="password py-2 px-4" placeholder="Buat Password Baru" />
+                  </label>
+                </div>
+              </form>
+            </div>
+          </div>
+        </section>
+
+        <div class="tab-content">
+          <p>Tab Buat Artikel</p>
+        </div>
+        <div class="tab-content">
+          <p>Tab Saya</p>
+        </div>
+      </main>
+    </div>
   </body>
+
+  <script src="assets/js/app.js"></script>
 </html>
